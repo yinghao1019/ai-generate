4d9c62a2d2da73c178f0d835c506ff947071e25f
Howard <z112517z@gmail.com>
2025-03-11 21:45:52 +0800

    feat:init commit

---
+++ src/main/java/com/howhow/aigenerate/AiGenerateApplication.java
@@ -0,0 +1,13 @@
+package com.howhow.aigenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGenerateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiGenerateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/howhow/aigenerate/AiGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howhow.aigenerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGenerateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
